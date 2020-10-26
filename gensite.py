059bb59ea7fcec1d6a35fb0590b43538b61bcd58
Arslan <rslnkrmt2552@gmail.com>
2020-10-26 22:25:52 +0300

    Making progress

--- gensite.py
+++ gensite.py
@@ -1,20 +1,35 @@
 import json
 
 from pathlib import Path
-
 from css_html_js_minify import html_minify
-from jinja2 import Template
+from jinja2 import (
+    Environment,
+    FileSystemLoader,
+    select_autoescape,
+    TemplateNotFound,
+)
 
-SOURCE = Path("./src")
+# Prepare Templates
+env = Environment(
+    loader=FileSystemLoader("src/templates"),
+    autoescape=select_autoescape(["html", "xml"]),
+)
 
-temp = Template((SOURCE / "_base.html").read_text())
-
+# Get Data
 with open("data.json", "r") as fp:
     data = json.load(fp)
 
-normal = temp.render(data)
-print(normal)
+# Render Templates
+root = Path("dist")
+if not root.exists():
+    root.mkdir()
 
-print("-" * 15)
-mini = html_minify(normal)
-print(mini)
+for f in Path("./src/templates").iterdir():
+    try:
+        temp = env.get_template(f.name)
+        rendered = temp.render(data)
+        root.joinpath(f.name).write_text(rendered)
+    except TemplateNotFound as e:
+        print(f"Template not found: {e}")
+    except Exception as e:
+        print(e)
